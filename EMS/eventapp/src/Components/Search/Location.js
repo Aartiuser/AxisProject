8b3ffd12cbca7f3ee17047a1e51d49ba6bf704df
Aartiuser <aarti231200@gmail.com>
2023-05-21 00:39:56 +0530

    Project Updated

--- EMS/eventapp/src/Components/Search/Location.js
+++ EMS/eventapp/src/Components/Search/Location.js
@@ -1,13 +1,96 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
+import star from "../../img/star.avif";
+import {MDBCard, MDBCardBody, MDBContainer, MDBInput} from "mdb-react-ui-kit";
+import UserService from "../../Services/UserService";
 
-class Location extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
+function Location(){
+    const[loc,setLoc]=useState('');
+    const [showForm, setShowForm] = useState(true);
+    const [showTable, setShowTable] = useState(false);
+    //const today = new Date().toISOString().substr(0, 10);
+    const[events,setevent]=useState([]);
+    const Search = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await UserService.getEventByloc(loc);
+            setevent(response.data);
+
+        } catch (error) {
+            console.error("Error fetching Events:", error);
+        }
+        setShowForm(false);
+        setShowTable(true);
+    };
+    const Close=()=>{
+        setShowTable(false);
+        window.location.replace("/UserHome");
     }
+    const Book=(event)=>{
+        const queryParams = new URLSearchParams({ event: JSON.stringify(event) });
+        window.location.replace(`/BookTicket?${queryParams.toString()}`);
+
+    }
+
+    return (
+        <div style={{ backgroundImage: `url(${star})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center', minHeight: '80vh' ,fontFamily:'Montserrat'}}>
+            <div>
+                {showForm &&(
+                    <MDBContainer fluid className='d-flex align-items-center justify-content-center ' >
+                        <div className='mask gradient-custom-3'></div>
+                        <MDBCard className='m-5' style={{maxWidth: '600px'}}>
+                            <MDBCardBody className='px-5'>
+                                <h2 className="text-uppercase text-center mb-5">Location</h2>
+
+                                <MDBInput wrapperClass='mb-1' label='Location' size='lg' id='loc' type='text'  value={loc}
+                                          onChange={(e) => setLoc(e.target.value )}
+                                />
+                                <button onClick={Search} className='mb-4 w-100 btn btn-dark'>Search Event</button>
+                            </MDBCardBody>
+                        </MDBCard>
+                    </MDBContainer>
+                )}
+            </div>
+            {showTable&&(
+                <div className="container custom-container" >
+                    <center>
+                        <h1 style={{ fontSize: '50px',fontFamily:'Montserrat' }}>Events in {loc}</h1>
+                        <div className="row">
+                            <table className='table table-striped table-bordered' style={{width: '100%', background: 'whitesmoke',fontFamily:'Montserrat' }} >
+                                <thead className='table-dark'>
+                                <tr>
+                                    <th style={{fontSize:'20px'}}><b>Event Title</b></th>
+                                    <th style={{fontSize:'20px'}}><b>Event Location</b></th>
+                                    <th style={{fontSize:'20px'}}><b>Event Start Date</b></th>
+                                    <th style={{fontSize:'20px'}}><b>Event End Date</b></th>
+                                    <th style={{fontSize:'20px'}}><b>Event Description</b></th>
+                                    <th style={{fontSize:'20px'}}><b>Actions</b></th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                {events.map( event =>(
+                                    <tr key={event.id} >
+                                        <td style={{fontSize:'20px'}}>{event.name}</td>
+                                        <td style={{fontSize:'20px'}}>{event.location}</td>
+                                        <td style={{fontSize:'20px'}}>{event.sdate}</td>
+                                        <td style={{fontSize:'20px'}}>{event.edate}</td>
+                                        <td style={{fontSize:'20px'}}>{event.description}</td>
+                                        <td>
+                                            <div className="button-container">
+                                                <button onClick={() => Book(event)} className="btn btn-info" style={{ fontFamily: 'Montserrat', marginLeft: "10px", marginTop: "10px", whiteSpace: "nowrap" }}>BOOK TICKET</button>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                ))}
+                                </tbody>
+                            </table>
+                            <button style={{ marginLeft: "10px" , marginTop:"10px",fontFamily:'Montserrat'}} onClick={() => Close()} className="btn btn-danger">CLOSE</button>
+                        </div>
+                    </center>
+                </div>
+            )}
+
+        </div>
+    );
 }
 
 export default Location;
